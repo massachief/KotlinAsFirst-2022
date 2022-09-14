007db0f1aa5136bfc59b188ec64aa3610a1de01f
Massachef <stronzinvilho@gmail.com>
2022-09-15 01:23:57 +0300

    Simple.kt

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -66,8 +66,7 @@ fun main() {
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
 fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
-    val seconds = hours * 3600 + minutes * 60 + seconds
-    return seconds
+    return  = hours * 3600 + minutes * 60 + seconds
 
 }
 
