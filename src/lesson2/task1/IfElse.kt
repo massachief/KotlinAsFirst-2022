f1b2a6a2071e7886dc4f973a43870380919024ad
Massachef <stronzinvilho@gmail.com>
2022-09-16 07:39:29 +0300

    lesson 2

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -68,7 +68,14 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String =
+    when {
+        age % 100 in 5..20 -> "$age лет"
+        age % 10 == 1 -> "$age год"
+        age % 10 in 2..4 -> "$age года"
+        age % 10 in 5..9 -> "$age лет"
+        else -> "$age лет"
+    }
 
 /**
  * Простая (2 балла)
@@ -81,7 +88,7 @@ fun timeForHalfWay(
     t1: Double, v1: Double,
     t2: Double, v2: Double,
     t3: Double, v3: Double
-): Double = TODO()
+): Double = 
 
 /**
  * Простая (2 балла)
